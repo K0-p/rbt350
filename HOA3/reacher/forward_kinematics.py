7f5ece4320dba6d41abdb7b990b382a1b3472ccf
Gabriel <gabrielalexandarmoore@gmail.com>
2024-04-13 14:37:59 -0500

    init

---
+++ HOA3/reacher/forward_kinematics.py
@@ -0,0 +1,122 @@
+import math
+import numpy as np
+import copy
+
+HIP_OFFSET = 0.0335
+UPPER_LEG_OFFSET = 0.10 # length of link 1
+LOWER_LEG_OFFSET = 0.13 # length of link 2
+
+def rotation_matrix(axis, angle):
+  """
+  Create a 3x3 rotation matrix which rotates about a specific axis
+
+  Args:
+    axis:  Array.  Unit vector in the direction of the axis of rotation
+    angle: Number. The amount to rotate about the axis in radians
+
+  Returns:
+    3x3 rotation matrix as a numpy array
+  """
+
+  axis = np.asarray(axis)
+  axis = axis / np.linalg.norm(axis)  # Normalize axis vector
+  a = np.cos(angle / 2.0)
+  b, c, d = -axis * np.sin(angle / 2.0)
+  aa, bb, cc, dd = a * a, b * b, c * c, d * d
+  bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+
+  # Construct rotation matrix
+  rot_mat = np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+    
+  #rot_mat = np.eye(3)
+  return rot_mat
+
+def homogenous_transformation_matrix(axis, angle, v_A):
+  """
+  Create a 4x4 transformation matrix which transforms from frame A to frame B
+
+  Args:
+    axis:  Array.  Unit vector in the direction of the axis of rotation
+    angle: Number. The amount to rotate about the axis in radians
+    v_A:   Vector. The vector translation from A to B defined in frame A
+
+  Returns:
+    4x4 transformation matrix as a numpy array
+  """
+
+  T = np.eye(4)
+  return T
+
+def fk_hip(joint_angles):
+  """
+  Use forward kinematics equations to calculate the xyz coordinates of the hip
+  frame given the joint angles of the robot
+
+  Args:
+    joint_angles: numpy array of 3 elements stored in the order [hip_angle, shoulder_angle, 
+                  elbow_angle]. Angles are in radians
+  Returns:
+    4x4 matrix representing the pose of the hip frame in the base frame
+  """
+
+  hip_frame = np.eye(4)  # remove this line when you write your solution
+  return hip_frame
+
+def fk_shoulder(joint_angles):
+  """
+  Use forward kinematics equations to calculate the xyz coordinates of the shoulder
+  joint given the joint angles of the robot
+
+  Args:
+    joint_angles: numpy array of 3 elements stored in the order [hip_angle, shoulder_angle, 
+                  elbow_angle]. Angles are in radians
+  Returns:
+    4x4 matrix representing the pose of the shoulder frame in the base frame
+  """
+
+  # remove these lines when you write your solution
+  default_sphere_location = np.array([[0.15, 0.0, -0.1]])
+  shoulder_frame = np.block(
+    [[np.eye(3), default_sphere_location.T], 
+     [0, 0, 0, 1]])
+  return shoulder_frame
+
+def fk_elbow(joint_angles):
+  """
+  Use forward kinematics equations to calculate the xyz coordinates of the elbow
+  joint given the joint angles of the robot
+
+  Args:
+    joint_angles: numpy array of 3 elements stored in the order [hip_angle, shoulder_angle, 
+                  elbow_angle]. Angles are in radians
+  Returns:
+    4x4 matrix representing the pose of the elbow frame in the base frame
+  """
+
+  # remove these lines when you write your solution
+  default_sphere_location = np.array([[0.15, 0.1, -0.1]])
+  elbow_frame = np.block(
+    [[np.eye(3), default_sphere_location.T], 
+     [0, 0, 0, 1]])
+  return elbow_frame
+
+def fk_foot(joint_angles):
+  """
+  Use forward kinematics equations to calculate the xyz coordinates of the foot given 
+  the joint angles of the robot
+
+  Args:
+    joint_angles: numpy array of 3 elements stored in the order [hip_angle, shoulder_angle, 
+                  elbow_angle]. Angles are in radians
+  Returns:
+    4x4 matrix representing the pose of the end effector frame in the base frame
+  """
+
+  # remove these lines when you write your solution
+  default_sphere_location = np.array([[0.15, 0.2, -0.1]])
+  end_effector_frame = np.block(
+    [[np.eye(3), default_sphere_location.T], 
+     [0, 0, 0, 1]])
+  return end_effector_frame
